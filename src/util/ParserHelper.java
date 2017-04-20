f31c30c8ef5ea7930ac83d1565aa265be2c4c2dc
Branislava Ivkovic <branislavaivkovic@Branislavas-MacBook-Pro.local>
2017-04-20 17:38:58 +0200

    Parser complete with counting.

--- src/util/ParserHelper.java
+++ src/util/ParserHelper.java
@@ -2,15 +2,48 @@ package util;
 
 public class ParserHelper {
 
+	private int globalVarCnt = 0;
+	private int localVarCnt = 0;
+	private int globalConstCnt = 0;
+	private int globalArrayCnt = 0;
+	
 	private boolean inProgram = false;
 	private boolean inMain = false;
 	
 	public boolean isInProgram() { return inProgram; }
-	public boolean inInMain() { return inMain; }
+	public boolean isInMain() { return inMain; }
 	
 	public void enteringProgram() { inProgram = true; }
 	public void exitingProgram() { inProgram = false; }
 	
 	public void enteringMain() { inMain = true; }
 	public void exitingMain() { inMain = false; }
+
+	public void globalVarFound() { globalVarCnt++; }
+	public void localVarFound() { localVarCnt++; }
+	public void globalConstFound() { globalConstCnt++; }
+	public void globalArrayFound() { globalArrayCnt++; }
+	
+	public int getGlobalVarCnt() {
+		return globalVarCnt;
+	}
+	public int getLocalVarCnt() {
+		return localVarCnt;
+	}
+	public int getGlobalConstCnt() {
+		return globalConstCnt;
+	}
+	public int getGlobalArrayCnt() {
+		return globalArrayCnt;
+	}
+	
+	public String printParseCount() {
+		String retVal = "";
+		retVal += "Global variable count: " + globalVarCnt + "\n";
+		retVal += "Local variable count: " + localVarCnt + "\n";
+		retVal += "Global array count: " + globalArrayCnt + "\n";
+		retVal += "Global constant count: " + globalConstCnt + "\n";
+		return retVal;
+	}
+	
 }
---
+++ test/source/ParserTest.java
@@ -0,0 +1,46 @@
+package source;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import java_cup.runtime.Symbol;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import util.Log4JUtils;
+
+public class ParserTest {
+
+	static {
+		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
+		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		Logger log = Logger.getLogger(ParserTest.class);
+		
+		Reader br = null;
+		try {
+			File sourceCode = new File("test/parserTest.mj");
+			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
+			
+			br = new BufferedReader(new FileReader(sourceCode));
+			Yylex lexer = new Yylex(br);
+			
+			MJParser p = new MJParser(lexer);
+	        p.parse();  //pocetak parsiranja
+	        
+	        //Ovde ide ispis
+	        System.out.println(p.parserHelper.printParseCount());
+		} 
+		finally {
+			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
+		}
+
+	}
+}
